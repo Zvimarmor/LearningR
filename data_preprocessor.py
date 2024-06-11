d01405b3fe8b2c022f15fd9422a5a7f530d826af
Zvi Marmor <Zvi.marmor@mail.huji.ac.il>
2024-06-11 16:34:12 +0300

    another changes in the data proccessor

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv('GSE208784_series_matrix.txt', sep='\t', skiprows=23)
+data = pd.read_csv('GSE208783_series_matrix.txt', sep='\t', skiprows=40)
 data = data.dropna()
 data = data.transpose()
 
@@ -19,10 +19,8 @@ data.drop('Sample_data_processing', axis=1, inplace=True)
 data.drop('Sample_relation', axis=1, inplace=True)
 
 
-
-
 #write the data to a new txt file
-data.to_csv('processed_data.txt', sep='\t', index=False)
+data.to_csv('processed_data1.txt', sep='\t', index=False)
 
 
 
